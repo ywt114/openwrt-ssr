70fbeb5e121ff175bae66cf11f60eaa27be78760
yuwentao114 <49578217+yuwentao114@users.noreply.github.com>
2020-06-17 18:15:54 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,4 +2,3 @@
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/#src-git helloworld/src-git helloworld/g' openwrt/feeds.conf.default
