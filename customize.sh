023dee7034faef9dde750eedcb9bd4d1c5534d66
ywt114 <58467878+ywt114@users.noreply.github.com>
2020-10-14 16:12:38 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' openwrt/package/lean/default-settings/files/zzz-default-settings
